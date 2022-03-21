b13b43976a6a9fc8395d64bdf64f99412a67a915
William G. Tresselt <williagt@stud.ntnu.no>
2022-03-21 09:56:00 +0100

    Prepping for implementation of MainServer

--- onion-routing/client/MainServer.cpp
+++ onion-routing/client/MainServer.cpp
@@ -7,3 +7,15 @@
 void generateKeys(){
 
 }
+
+int MainServer::start() {
+    //create nodePool with x Nodes and give them their
+    //ask for nodeAmount, at least 3
+    //pick nodeAmount Nodes from pool
+    //initiate server socket and forward socket
+    //inform user about what to do next (log on with browser)
+    //when user logged on, send welcome message. 'type localhost:x/help for user manual' TODO need parser
+    //if user sends in something else than preprogrammed cases, send to InputNode from userNodes vector and hope for the best
+    //enter 'localhost:x/exit' to terminate program
+    return 0;
+}
--- onion-routing/client/MainServer.h
+++ onion-routing/client/MainServer.h
@@ -7,16 +7,27 @@
 
 #include "vector"
 #include "../node/Node.h"
+#include "../node/InputNode.h"
+#include "../node/ExitNode.h"
+
 using namespace std;
 class MainServer {
 private:
+    vector <Node> nodePool;
+    vector <InputNode> inputNodePool;
+    vector <ExitNode> exitNodePool;
+
+    SOCKET server; //Client connects to this one through browser
+    SOCKET forward; //Used to forward information to Nodes
+
+    int nodeAmount; //Amount of nodes
+    vector <Node> userNodes;
     vector <Key> keys;
-    Node node; //Byttes ut med socket
 public:
 
-    void generateKeys(){
-
-    }
+    void generateKeys();
+    int start();
+    string parseGetReq();
 };
 
 
